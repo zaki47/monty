ff24bc3d53e41b348cce0566f412a86da238b57f
Zakaria TOURAS <zakari4touras@gmail.com>
2024-02-01 17:43:37 -0800

    noop

---
+++ noop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - do no operation
+ * @stack: head
+ * @line_nop: line
+ */
+
+void nop(stack_t **stack, unsigned int line_nop)
+{
+	(void)stack;
+	(void)line_nop;
+}
