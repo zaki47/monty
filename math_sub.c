9ff546bb49428c292d2f1df7c7c9202df2ff4e32
Zakaria TOURAS <zakari4touras@gmail.com>
2024-02-01 18:01:38 -0800

    math_sub

---
+++ math_sub.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * sub - add two nodes & pop the last.
+ *
+ * @stack: head
+ * @line: line
+ * Return: void
+ */
+
+void sub(stack_t **stack, uint line)
+{
+	int subnum;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+	subnum = (*stack)->n - (*stack)->next->n;
+	pop_stack(stack, line);
+	(*stack)->n = subnum;
+}
+
